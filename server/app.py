d5d0b86995388a3cdaf67bfce5e82c15907c7c86
sabundhal <sabundhal@gmail.com>
2025-02-11 21:52:48 +0300

    12 переделан метод расчета и вынесен в отдельный файл

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from flask_cors import CORS
 import sqlite3
 import hashlib
+from antipyretic_calculator import calculateAntipyreticDosage
 
 ##
 # Создание Flask-приложения
@@ -323,20 +324,6 @@ def register_user():
 
 
 
-# Вход пользователя
-#@app.route('/api/login', methods=['POST'])
-#def login_user():
- #   data = request.get_json()
-  #  username = data.get('username')
-   # password = data.get('password')
-    #if not username or not password:
-     #   return jsonify({'status': 'error', 'message': 'Username and password are required'}), 400
-    #user = next((user for user in USERS if user['username'] == username and user['password'] == password), None)
-    #if user:
-     #   access_token = create_access_token(identity=username)  # Создание токена
-      #  return jsonify({'status': 'success', 'access_token': access_token}), 200
-    #else:
-     #   return jsonify({'status': 'error', 'message': 'Invalid username or password'}), 401
 
 
 @app.route('/api/login', methods=['POST'])
@@ -400,80 +387,91 @@ def get_drugs():
 
 ##Расчет дозировки и сохранение в БД@app.route('/calculate', methods=['POST'])
 @app.route('/api/calculate', methods=['POST'])
-#@jwt_required()  # Защищаем маршрут JWT-токеном
 def calculate_dosage():
     data = request.json
-    user_id = data.get('user_id')
-    drug_id = data.get('drug_id')
-    weight = data.get('weight')
-    weight = float(weight)
-
-    # Проверяем наличие обязательных параметров
-    if user_id is None or drug_id is None or weight is None:
-        return jsonify({'error': 'user_id, drug_id, and weight are required'}), 400
-
-    # Проверяем корректность веса
-    if weight <= 0:
-        return jsonify({'error': 'Weight must be greater than 0'}), 400
-
-    # Получаем данные о препарате из базы данных
     try:
-        conn = sqlite3.connect('myapp.db')
-        cursor = conn.cursor()
-
-        # Запрос данных о препарате
-        #можно ли тут написать. select *?
-        cursor.execute('''SELECT name, category_id, mls_var, mgs_var
-                          FROM drugs
-                          WHERE id = ?''', (drug_id,))
-        drug = cursor.fetchone()
-
-        if not drug:
-            conn.close()
-            return jsonify({'error': 'Drug not found'}), 404
-
-        # Извлекаем данные из результата запроса
-        name, category_id, mls_var, mgs_var = drug
-
-        # Выполняем расчеты
-        mls_total = weight * mls_var
-        mgs_total = weight * mgs_var
-
-        # Сохраняем расчет в историю
-        # Установка значений для всех 23 полей
-        cursor.execute('''
-            INSERT INTO calculation_history (
-                user_id, calculation_id, drug_id, drug_name, username, weight, dosage_mls, dosage_mgs,
-                totalMgs, totalhigh, totalhighsachets, maximumMgsPerDay, highMgs,
-                loading_dose, strep_drug, messageMgs, messageOther, calculation_type,
-                calculation_status, calculation_version, patient_id, patient_name, error_message, age
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)''',
-                       (
-                           user_id, None, drug_id, name, 'Username', weight, mls_total, mgs_total,
-                           0, 0, 0, 0, 0, 0, 0, 'Message', None, category_id,
-                           'Status', 'Version', 0, 'Patient Name', None , None
-                       ))
-        # Получаем автоматически сгенерированный id
-        calculation_id = cursor.lastrowid
-        # Обновляем запись, чтобы установить calculation_id равным id
-        cursor.execute('''
-            UPDATE calculation_history
-            SET calculation_id = ?
-            WHERE id = ?''',
-                       (calculation_id, calculation_id))
-        conn.commit()
+        result = calculateAntipyreticDosage(data)
+        return jsonify(result)
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
 
 
-    except sqlite3.Error as e:
-        return jsonify({'error': f'Database error: {str(e)}'}), 500
 
-        # Возвращаем ответ с calculation_id
-    return jsonify({
-            'mlsTotal': mls_total,
-            'mgsTotal': mgs_total,
-            'calculation_id': calculation_id
-        })
-    conn.close()
+# @app.route('/api/calculate', methods=['POST'])
+# #@jwt_required()  # Защищаем маршрут JWT-токеном
+# def calculate_dosage():
+#     data = request.json
+#     user_id = data.get('user_id')
+#     drug_id = data.get('drug_id')
+#     weight = data.get('weight')
+#     weight = float(weight)
+#
+#     # Проверяем наличие обязательных параметров
+#     if user_id is None or drug_id is None or weight is None:
+#         return jsonify({'error': 'user_id, drug_id, and weight are required'}), 400
+#
+#     # Проверяем корректность веса
+#     if weight <= 0:
+#         return jsonify({'error': 'Weight must be greater than 0'}), 400
+#
+#     # Получаем данные о препарате из базы данных
+#     try:
+#         conn = sqlite3.connect('myapp.db')
+#         cursor = conn.cursor()
+#
+#         # Запрос данных о препарате
+#         #можно ли тут написать. select *?
+#         cursor.execute('''SELECT name, category_id, mls_var, mgs_var
+#                           FROM drugs
+#                           WHERE id = ?''', (drug_id,))
+#         drug = cursor.fetchone()
+#
+#         if not drug:
+#             conn.close()
+#             return jsonify({'error': 'Drug not found'}), 404
+#
+#         # Извлекаем данные из результата запроса
+#         name, category_id, mls_var, mgs_var = drug
+#
+#         # Выполняем расчеты
+#         mls_total = weight * mls_var
+#         mgs_total = weight * mgs_var
+#
+#         # Сохраняем расчет в историю
+#         # Установка значений для всех 23 полей
+#         cursor.execute('''
+#             INSERT INTO calculation_history (
+#                 user_id, calculation_id, drug_id, drug_name, username, weight, dosage_mls, dosage_mgs,
+#                 totalMgs, totalhigh, totalhighsachets, maximumMgsPerDay, highMgs,
+#                 loading_dose, strep_drug, messageMgs, messageOther, calculation_type,
+#                 calculation_status, calculation_version, patient_id, patient_name, error_message, age
+#             ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)''',
+#                        (
+#                            user_id, None, drug_id, name, 'Username', weight, mls_total, mgs_total,
+#                            0, 0, 0, 0, 0, 0, 0, 'Message', None, category_id,
+#                            'Status', 'Version', 0, 'Patient Name', None , None
+#                        ))
+#         # Получаем автоматически сгенерированный id
+#         calculation_id = cursor.lastrowid
+#         # Обновляем запись, чтобы установить calculation_id равным id
+#         cursor.execute('''
+#             UPDATE calculation_history
+#             SET calculation_id = ?
+#             WHERE id = ?''',
+#                        (calculation_id, calculation_id))
+#         conn.commit()
+#
+#
+#     except sqlite3.Error as e:
+#         return jsonify({'error': f'Database error: {str(e)}'}), 500
+#
+#         # Возвращаем ответ с calculation_id
+#     return jsonify({
+#             'mlsTotal': mls_total,
+#             'mgsTotal': mgs_total,
+#             'calculation_id': calculation_id
+#         })
+#     conn.close()
 
 #############
 @app.route('/api/calculation-history/', methods=['GET'])
@@ -518,7 +516,7 @@ def get_calculation_history():
                 'id': row[0],  # id
         'user_id': row[2],  # user_id
         'drug_name': row[4],  # drug_name
-        'calculation_type': row[15],
+        'calculation_type': row[18],
         'weight': row[6],       # calculation_type
         'mls': row[7],  # dosage_mls
         'mgs': row[8],  # dosage_mgs
